ece7f023a466140ce831a2c6202a700a3d6f83f0
Edwin <edwinm87@gmail.com>
2021-05-02 16:08:25 -0600

    Added initial cards w/ javascript and html template

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,48 @@
-const editButton = document.querySelector('.profile__edit-button');
+//Declarations
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
 
+
+const cardGrid = document.querySelector(".cards__grid");
+
+const editButton = document.querySelector('.profile__edit-button');
+const titleValue = document.querySelector(".profile__title");
+const subtitleValue = document.querySelector(".profile__subtitle");
 
 const popup = document.querySelector('.popup');
-const exitButton = document.querySelector('.popup__exit-button');
+const exitButton = popup.querySelector('.popup__exit-button');
 
 const form = document.querySelector('.form');
-const submitButton = document.querySelector('.form__submit-button');
-
-
+const submitButton = form.querySelector('.form__submit-button');
 const titleInput = form.querySelector('.form__input_text_title');
 const subtitleInput = form.querySelector('.form__input_text_subtitle');
 
 
-const titleValue = document.querySelector(".profile__title");
-const subtitleValue = document.querySelector(".profile__subtitle");
-
-//Toggle Form Handlers
+//Functions
 const toggleForm = () => {
     popup.classList.toggle('popup_visible');
     if (popup.classList.contains('popup_visible')) {
@@ -24,10 +51,6 @@ const toggleForm = () => {
     }
 }
 
-editButton.addEventListener('click', toggleForm);
-exitButton.addEventListener('click', toggleForm);
-
-
 const fillForm = (evt) => {
     evt.preventDefault();
     titleValue.textContent = titleInput.value;
@@ -35,6 +58,31 @@ const fillForm = (evt) => {
     toggleForm();
 }
 
+const createCardElement = (card) => {
+    //Get a reference to template element
+    const cardTemplate = document.querySelector("#card-template").content;
+    //Clone content of card template
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    // Set Image and Title
+    cardElement.querySelector('.card__image').style.backgroundImage = `url(${card.link})`;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    return cardElement;
+}
+
+const renderCard = (card, wrapper) => {
+    const cardElement = createCardElement(card);
+    wrapper.append(cardElement);
+}
+
+
+
+// Handlers
+editButton.addEventListener('click', toggleForm);
+exitButton.addEventListener('click', toggleForm);
 form.addEventListener('submit', fillForm);
+
+
+initialCards.forEach(card => renderCard(card, cardGrid));
 
 
